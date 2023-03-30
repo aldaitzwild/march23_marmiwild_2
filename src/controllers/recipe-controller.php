433618b3f83d748321cec94a1f39e9b7662b78d3
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-30 12:20:39 +0200

    Add the step 2 of marmiwild

---
+++ src/controllers/recipe-controller.php
@@ -0,0 +1,50 @@
+<?php
+require __DIR__ . '/../models/recipe-model.php';
+
+function browseRecipes(): void
+{
+    $recipes = getAllRecipes();
+
+    require __DIR__.'/../views/index.php';
+}
+
+function showRecipe(int $id): void
+{
+    $recipe = getRecipeById($id);
+
+    if (!isset($recipe['title']) || !isset($recipe['description'])) {
+        header("Location: /");
+        exit("Recipe not found");
+    }
+
+    require __DIR__.'/../views/show.php';
+}
+
+function addRecipe(): void 
+{
+    $errors = [];
+
+    if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+        $recipe = array_map('trim', $_POST);
+
+        // Validate data
+        if (empty($recipe['title'])) {
+            $errors[] = 'The title is required';
+        }
+        if (empty($recipe['description'])) {
+            $errors[] = 'The description is required';
+        }
+        if (!empty($recipe['title']) && strlen($recipe['title']) > 255) {
+            $errors[] = 'The title should be less than 255 characters';
+        }
+
+        // Save the recipe
+        if (empty($errors)) {
+            saveRecipe($recipe);
+            header('Location: /');
+        }
+    }
+
+    // Generate the web page
+    require __DIR__ . '/../views/form.php';
+}
